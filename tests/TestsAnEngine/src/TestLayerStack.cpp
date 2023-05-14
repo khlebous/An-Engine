7a820adc36e22a4a8f841fa2fd8c728d4ba75f00
ana <khlebousa@gmail.com>
2023-05-14 19:40:34 +0200

    wip

--- tests/TestsAnEngine/src/TestLayerStack.cpp
+++ tests/TestsAnEngine/src/TestLayerStack.cpp
@@ -9,13 +9,13 @@ class TestLayerStack : public testing::Test
 
 TEST_F(TestLayerStack, empty)
 {
-    an::A aa;
+    //an::A aa;
 
 
     //an::LayerStack layerStack;
     //layerStack.begin();
     int a = 3;
-    EXPECT_EQ(3, a);
+   // EXPECT_EQ(3, a);
 
     //EXPECT_EQ(layerStack.begin(), layerStack.end());
 }
